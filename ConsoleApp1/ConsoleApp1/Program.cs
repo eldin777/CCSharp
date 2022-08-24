3555ff49e9d232ffd252adca3cdb5416187448e5
eldin777 <69908631+eldin777@users.noreply.github.com>
2022-08-24 16:09:57 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConsoleAppDemo
+{
+    internal class Words1
+    {
+        public static void Main()
+        {
+            int a = 0, w = 1;
+            string str = "Hello World!";
+            while (a <= str.Length - 1)
+            {
+                if (str[a] == ' ' || str[a] == '\n' || str[a] == '\t')
+                {
+                    w++;
+                }
+                a++;
+            }
+            Console.Write("Number of words in the string = {0}\n", w);
+        }
+    }
+}
+
